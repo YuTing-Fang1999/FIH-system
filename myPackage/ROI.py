17a902e8fa96c0c28b4c2fe1527e79b773ba1de5
YuTing-Fang1999 <4107056005@smail.nchu.edu.tw>
2022-07-13 17:28:23 +0800

    sharp test

--- myPackage/ROI.py
+++ myPackage/ROI.py
@@ -5,6 +5,7 @@ from scipy.signal import convolve2d
 import math
 from math import e
 
+
 class ROI:
     # 建構式
     def __init__(self):
@@ -16,23 +17,24 @@ class ROI:
         self.img = img
         self.roi_coordinate = roi_coordinate
         coor = self.roi_coordinate
-        self.roi_img = self.img[int(coor.r1):int(coor.r2), int(coor.c1):int(coor.c2), :]
-        
+        self.roi_img = self.img[int(coor.r1):int(
+            coor.r2), int(coor.c1):int(coor.c2), :]
 
     ###### sharpness ######
+
     def get_sharpness(self):
         I = self.roi_img.copy()
         I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY).astype('float64')
         return np.round(np.sqrt(cv2.Laplacian(I, cv2.CV_64F).var()), 4)
-    
-    def get_noise(self):  
+
+    def get_noise(self):
         I = self.roi_img.copy()
         I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY).astype('float64')
         H, W = I.shape
 
         M = [[1, -2, 1],
-           [-2, 4, -2],
-           [1, -2, 1]]
+             [-2, 4, -2],
+             [1, -2, 1]]
 
         sigma = np.sum(np.sum(np.absolute(convolve2d(I, M))))
         sigma = sigma * math.sqrt(0.5 * math.pi) / (6 * (W-2) * (H-2))
@@ -51,9 +53,9 @@ class ROI:
     def get_Imatest(self):
         I = self.roi_img.copy()
         I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY).astype('float64')
-        gamma=0.5
+        gamma = 0.5
         invGamma = 1.0 / gamma
-        I = np.array(((I / 255.0) ** invGamma) * 255) # linearized
+        I = np.array(((I / 255.0) ** invGamma) * 255)  # linearized
 
         sobelx = cv2.Sobel(I, cv2.CV_16S, 1, 0)  # x方向梯度 ksize默認為3x3
         sobely = cv2.Sobel(I, cv2.CV_16S, 0, 1)  # y方向梯度
@@ -63,7 +65,7 @@ class ROI:
 
         sx = np.mean(abx)/np.mean(I)
         sy = np.mean(aby)/np.mean(I)
-        
+
         sTotal = np.sqrt(sx*sx + sy*sy)
 
         return np.round(sTotal, 4)
@@ -71,9 +73,9 @@ class ROI:
     def get_gamma_Sobel(self):
         I = self.roi_img.copy()
         I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY).astype('float64')
-        gamma=0.5
+        gamma = 0.5
         invGamma = 1.0 / gamma
-        I = np.array(((I / 255.0) ** invGamma) * 255) # linearized
+        I = np.array(((I / 255.0) ** invGamma) * 255)  # linearized
 
         sobelx = cv2.Sobel(I, cv2.CV_16S, 1, 0)  # x方向梯度 ksize默認為3x3
         sobely = cv2.Sobel(I, cv2.CV_16S, 0, 1)  # y方向梯度
@@ -85,34 +87,33 @@ class ROI:
     def get_gamma_Laplacian(self):
         I = self.roi_img.copy()
         I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY).astype('float64')
-        gamma=0.5
+        gamma = 0.5
         invGamma = 1.0 / gamma
-        I = np.array(((I / 255.0) ** invGamma) * 255) # linearized
+        I = np.array(((I / 255.0) ** invGamma) * 255)  # linearized
 
         return np.round(np.sqrt(cv2.Laplacian(I, cv2.CV_64F, ksize=1).var()), 4)
 
     def get_H(self):
         I = self.roi_img.copy()
         I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY).astype('float64')
-        gamma=0.5
+        gamma = 0.5
         invGamma = 1.0 / gamma
-        I = np.array(((I / 255.0) ** invGamma) * 255) # linearized
+        I = np.array(((I / 255.0) ** invGamma) * 255)  # linearized
 
         gy, gx = np.gradient(I)
-        
-        h = np.sqrt(gx.var())
-        return np.round(np.sqrt((h**2 + h**2)/2), 4)
+
+        return np.round(gx.std(), 4)
 
     def get_V(self):
         I = self.roi_img.copy()
         I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY).astype('float64')
-        gamma=0.5
+        gamma = 0.5
         invGamma = 1.0 / gamma
-        I = np.array(((I / 255.0) ** invGamma) * 255) # linearized
+        I = np.array(((I / 255.0) ** invGamma) * 255)  # linearized
 
         gy, gx = np.gradient(I)
 
-        return np.round(np.sqrt(gy.var()), 4)
+        return np.round(gy.std(), 4)
 
     ############ DXO deadleaves #############
 
@@ -138,7 +139,7 @@ class ROI:
 
         # 找出半徑改變的位置 rind=[0,8] 代表在0~1、8~9之間改變 => 0, 1~8, 9~24
         deltar = r_int - np.roll(r_int, -1)  # shift and substract
-        rind = np.where(deltar!=0)[0]       # location of changed radius
+        rind = np.where(deltar != 0)[0]       # location of changed radius
 
         # 對陣列的值做累加
         csim = np.cumsum(i_sorted, dtype=float)
@@ -154,7 +155,6 @@ class ROI:
 
         return tbin
 
-    
     def get_DXO_accurate(self):
 
         # read img
@@ -166,10 +166,11 @@ class ROI:
         N = min(I.shape)
 
         # let N be the odd number
-        if N%2==0: N-=1
-        I = I[:N, :N] 
+        if N % 2 == 0:
+            N -= 1
+        I = I[:N, :N]
 
-        ############################ compute I_hat(m, n) 
+        # compute I_hat(m, n)
 
         # Take the fourier transform of the image.
         I_hat = np.fft.fft2(I)
@@ -177,7 +178,7 @@ class ROI:
         # shift
         # [-N/2, N/2] => [0, N]
         # I(0,0) => I(N//2, N//2)
-        I_hat = np.fft.fftshift(I_hat) 
+        I_hat = np.fft.fftshift(I_hat)
 
         # get the real part
         I_hat = np.abs(I_hat)
@@ -188,59 +189,60 @@ class ROI:
 
         # print(I_hat[N//2-1:N//2+2, N//2-1:N//2+2])
 
-        ############################ compute c(N)
+        # compute c(N)
 
         eta = -1.93
         Denominator = 0
         for m in range(0, N):
             for n in range(0, N):
-                if m==N//2 and n == N//2: continue
+                if m == N//2 and n == N//2:
+                    continue
                 Denominator += (1 / pow(((m-N//2)**2 + (n-N//2)**2), eta/2))
         cN = (I.var() / Denominator) * (N**4)
 
-        ############################ compute T_hat(m, n)
+        # compute T_hat(m, n)
 
-        T_hat = np.zeros((N,N))
+        T_hat = np.zeros((N, N))
         for m in range(0, N):
             for n in range(0, N):
-                if m==N//2 and n == N//2: continue
-                T_hat[m,n] = cN / ((m-N//2)**2 + (n-N//2)**2)**(eta/2)
+                if m == N//2 and n == N//2:
+                    continue
+                T_hat[m, n] = cN / ((m-N//2)**2 + (n-N//2)**2)**(eta/2)
         # when m==0 and n == 0
-        T_hat[N//2,N//2] = I_hat[N//2,N//2]
+        T_hat[N//2, N//2] = I_hat[N//2, N//2]
 
-        ############################ compute K(m, n)
+        # compute K(m, n)
 
-        K = I_hat / T_hat 
+        K = I_hat / T_hat
         # print(K[N//2, N//2])
 
-        ############################ compute MTF
+        # compute MTF
 
         # The one-dimensional texture MTF is the average of over all directions.
         MTF = self.radialAverage(K)
         print(MTF[:10])
 
-        ############################ compute CSF
+        # compute CSF
 
         # contrast sensitivity function (CSF) can be used to weigh the
         # different spatial frequencies, leading to a single acutance value
         b = 0.2
         c = 0.8
         # CSF(v) = a * pow(v, c) * pow(e, -b*v)
-        # ∫ CSF(v) dv = 1 
+        # ∫ CSF(v) dv = 1
         # ∫ a * pow(v, c) * pow(e, -b*v) dv = 1
         # a * ∫ pow(v, c) * pow(e, -b*v) dv = 1
         # a = 1 / ∫ pow(v, c) * pow(e, -b*v) dv
-        a = 1 / np.sum([ pow(v, c) * pow(e, -b*v) for v in range(MTF.shape[0])])
-        CSF = [ a * pow(v, c) * pow(e, -b*v) for v in range(MTF.shape[0])]
+        a = 1 / np.sum([pow(v, c) * pow(e, -b*v) for v in range(MTF.shape[0])])
+        CSF = [a * pow(v, c) * pow(e, -b*v) for v in range(MTF.shape[0])]
 
-        ############################ compute Acutance
+        # compute Acutance
 
-        A = np.sum([ MTF[v] * CSF[v] for v in range(MTF.shape[0])])
+        A = np.sum([MTF[v] * CSF[v] for v in range(MTF.shape[0])])
         # print(A)
 
         return np.round(A, 4)
 
-    
     # ###### colorcheck ######
     # def gen_colorcheck_coordinate(self):
     #     coor = []
@@ -269,5 +271,3 @@ class ROI:
     #     for i in range(24):
     #         cv2.rectangle(img, (self.roi_coordinate[i][0], self.roi_coordinate[i][1]), (self.roi_coordinate[i][2], self.roi_coordinate[i][3]), color, thickness)
     #     return img
-
-    
--- test img/sharp.py
+++ test img/sharp.py
@@ -1,8 +1,9 @@
+import pandas as pd
 import cv2
 import numpy as np
 from scipy.signal import convolve2d
 
-img = cv2.imread("test_grid.jpg")
+img = cv2.imread("test4.jpg")
 
 I = img.copy()
 I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY).astype('float64')
@@ -10,90 +11,48 @@ gamma = 0.5
 invGamma = 1.0 / gamma
 I = np.array(((I / 255.0) ** invGamma) * 255)  # linearized
 
-# I = np.ones([4,4])
-# I[1:3,1:3] = 0
-# print(I)
+n = 2
+# print(n)
+kernal_x = np.array([
+    [-1, 0, 1],
+])/n
+
+kernal_y = np.array([
+    [-1],
+    [0],
+    [1],
+])/n
+
+H = cv2.filter2D(I, cv2.CV_64F, kernal_x)
+V = cv2.filter2D(I, cv2.CV_64F, kernal_y)
+# print(H.std())
+# print(V.std())
+# print(np.sqrt((H.var() + V.var())/2))
 
 
-g = cv2.Laplacian(I, cv2.CV_64F, ksize=1)
+# n = 8
+# print(n)
+# kernal_x = np.array([
+#     [-1,  0,  1],
+#     [-2,  0,  2, ],
+#     [-1,  0,  1, ]
+# ])/n
 
-v = np.round(g.var(), 4)
-
-print(v)
-print()
-
-
-sobelx = cv2.Sobel(I, cv2.CV_64F, 1, 0, ksize=1)  # x方向梯度 ksize默認為3x3
-sobely = cv2.Sobel(I, cv2.CV_64F, 0, 1, ksize=1)  # y方向梯度
-
-abx = np.abs(sobelx)  # 取sobelx絕對值
-aby = np.abs(sobely)  # 取sobely絕對值
-# print(sobelx.var()+sobely.var())
-# print(sobely.var())
-# print()
-
-# print(sobelx.var())
-# print(np.mean(sobelx**2))
-
-
-# # 拉普拉斯濾波器
-# laplace_filter = np.array([
-#     [0, 1, 0],
-#     [1, -4, 1],
-#     [0, 1, 0],
-# ])
-
-# laplace_filter = np.array([
+# kernal_y = np.array([
 #     [-1, -2, -1],
-#     [ 0,  0,  0,],
-#     [ 1,  2,  1,]
-# ])
+#     [0,  0,  0, ],
+#     [1,  2,  1, ]
+# ])/n
 
-# laplace_filter = np.array([
-#     [-1],
-#     [ 0],
-#     [ 1]
-# ])
+H = cv2.filter2D(I, cv2.CV_64F, kernal_x)
+V = cv2.filter2D(I, cv2.CV_64F, kernal_y)
 
-laplace_filter = np.array([
-    [-1, -2, -1],
-    [0,  0,  0, ],
-    [1,  2,  1, ]
-])
+df_describe = pd.DataFrame(H.reshape(-1))
+print(df_describe.describe())
 
-
-g = cv2.filter2D(I, cv2.CV_64F, laplace_filter)
-# v = np.round(np.sqrt(g.var()), 4)
-# print(g)
-# print(np.abs(g).mean())
-print(g.var())
-
-# sobel5x = cv2.getDerivKernels(2, 0, 3)
-# print(np.outer(sobel5x[0], sobel5x[1]))
-
-img = np.zeros([1000, 1000])
-
-# for i in range(0,10,2):
-#     img[i*100:(i+1)*100] = 255
-
-img[500:, :] = 255
-
-mean = 0
-sigma = 0.1
-# int -> float (標準化)
-img = img / 255
-# 隨機生成高斯 noise (float + float)
-noise = np.random.normal(mean, sigma, img.shape)
-# noise + 原圖
-gaussian_out = img + noise
-# 所有值必須介於 0~1 之間，超過1 = 1，小於0 = 0
-gaussian_out = np.clip(gaussian_out, 0, 1)
-
-# 原圖: float -> int (0~1 -> 0~255)
-gaussian_out = np.uint8(gaussian_out*255)
-
-cv2.imshow('img', gaussian_out)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cv2.imwrite('test_grid2.jpg', gaussian_out)
+# H = H[np.abs(H) > 0.1]
+# V = V[np.abs(V) > 0.1]
+print(H.std())
+print(V.std())
+# print(np.sqrt((H.var() + V.var())/2))
+# H = H[H > 0.1]
